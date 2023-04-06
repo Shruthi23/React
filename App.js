3a2424333ae2171e26c9550f9196286af612a075
Shruthi23 <shruthisrinivasan97@gmail.com>
2023-04-06 13:19:54 -0700

    Basic HTML

---
+++ App.js
@@ -0,0 +1,6 @@
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am H1 tag"),React.createElement("h2",{},"I am H2 tag")]))
+const heading = React.createElement("h1",{id:"heading"},"Hello from React!")
+console.log("heading",heading)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+console.log("root",parent)
+ root.render(parent)
